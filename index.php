c92a2bdda560d1c466b71051e6b2a715d84caf29
Aisha <115123499+AishaBaliyan@users.noreply.github.com>
2024-09-30 15:23:17 +0000

    fixed mdl button

--- index.php
+++ index.php
@@ -1,51 +1,37 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
   <link rel="manifest" href="favicons/site.webmanifest">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Your Page Title</title>
 </head>
 
 <body>
-  <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-  <link rel="manifest" href="favicons/site.webmanifest">
-  <form action="welcome.php" method="post">
-    <div id="form-main">
-      <div id="form-div">
-        <form class="form" id="form1">
-
-          <p class="name">
-          <form action="welcome.php" method="POST">
-            <p class="name">
-              <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name" id="name" />
-            </p>
-          </form>
-
-          <p class="email">
-            <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" />
-          </p>
-
-          <p class="text">
-            <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comment"></textarea>
-          </p>
-
-
-
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-            submit
-          </button>
-      </div>
-  </form>
+  <div id="form-main">
+    <div id="form-div">
+      <form action="welcome.php" method="POST" class="form" id="form1">
+        <p class="name">
+          <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name" id="name" required />
+        </p>
+        <p class="email">
+          <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" required />
+        </p>
+        <p class="text">
+          <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comment" required></textarea>
+        </p>
+        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
   </div>
-  </div>
-  </form>
-  </div>
-  </form>
-
 </body>
 
 </html>
