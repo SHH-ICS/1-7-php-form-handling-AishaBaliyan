27f83d63053dc8e982a3d088ebbe7f8b69d92366
Aisha <115123499+AishaBaliyan@users.noreply.github.com>
2024-09-30 14:33:21 +0000

    commit

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <!DOCTYPE HTML>
 <html>
+<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+<link rel="manifest" href="favicons/site.webmanifest">
 
 <body>
-
+  <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+  <link rel="manifest" href="favicons/site.webmanifest">
   <form action="welcome.php" method="post">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
+    <div id="form-main">
+      <div id="form-div">
+        <form class="form" id="form1">
+
+          <p class="name">
+          <form action="welcome.php" method="POST">
+            <p class="name">
+              <input name="name" type="text" class="validate[required,custom[onlyLetter],length[0,100]] feedback-input" placeholder="Name" id="name" />
+            </p>
+          </form>
+
+          <p class="email">
+            <input name="email" type="text" class="validate[required,custom[email]] feedback-input" id="email" placeholder="Email" />
+          </p>
+
+          <p class="text">
+            <textarea name="text" class="validate[required,length[6,300]] feedback-input" id="comment" placeholder="Comment"></textarea>
+          </p>
+
+
+        
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+              submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </form>
+  </div>
   </form>
 
 </body>
--- welcome.php
+++ welcome.php
@@ -7,10 +7,10 @@
   <title>Page Title</title>
 
   <!-- Favicon links -->
-  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-  <link rel="manifest" href="favicon/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+  <link rel="manifest" href="favicons/site.webmanifest">
 
   <!-- Material Design Lite links -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -19,46 +19,37 @@
 </head>
 
 <body>
-  <h1>Welcome to my Awesome Webpage</h1>
 
   <!-- Simple header with scrollable tabs -->
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Enter your name: &nbsp;&nbsp;</span>
-
         <form action="#">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="text" id="sample1">
-            <label class="mdl-textfield__label" for="sample1">Text</label>
-          </div>
         </form>
-
-        <button class="mdl-button mdl-js-button mdl-button--raised" onclick="window.location.href = 'tabone.html';">
-          Submit
-        </button>
       </div>
     </header>
-
-    <!-- Other content -->
-    <div class="mdl-layout__tab-bar mdl-js-ripple-effect">
-      <a href="#scroll-tab-1" class="mdl-layout__tab is-active">Red</a>
-      <a href="#scroll-tab-2" class="mdl-layout__tab">Purple</a>
-      <a href="#scroll-tab-3" class="mdl-layout__tab">Pink</a>
-      <a href="#scroll-tab-4" class="mdl-layout__tab">Blue</a>
-      <a href="#scroll-tab-5" class="mdl-layout__tab">Green</a>
-      <a href="#scroll-tab-6" class="mdl-layout__tab">Yellow</a>
-    </div>
-
     <main class="mdl-layout__content">
       <section class="mdl-layout__tab-panel is-active" id="scroll-tab-1">
         <div class="page-content">
-          <button onclick="window.location.href = 'tabone.html';">CLICK MEEEE</button>
+
+          <!-- Insert PHP code here to display the name -->
+          <h1>Welcome </h1>
+          <?php
+          if (isset($_POST['name'])) {
+            // Sanitize the input to avoid XSS attacks
+            $name = htmlspecialchars($_POST['name']);
+            echo "<p>Hello, " . $name . "!</p>";
+          } else {
+            echo "<p>No name provided!</p>";
+          }
+          ?>
+
         </div>
       </section>
       <!-- Additional sections for other tabs -->
     </main>
   </div>
+
 </body>
 
 </html>
